e033a244b73731aa18275d127757f7dab276cba1
machineminded <155763297+machineminded@users.noreply.github.com>
2024-01-22 15:39:26 -0600

    clean up preview stuff

--- modules/pm.py
+++ modules/pm.py
@@ -3,23 +3,24 @@ import os
 import sys
 sys.path.append('../photomaker')
 
-from diffusers import EulerDiscreteScheduler,  DPMSolverMultistepScheduler
+from diffusers import EulerDiscreteScheduler,  DPMSolverMultistepScheduler, AutoencoderKL
 from photomaker import PhotoMakerStableDiffusionXLPipeline
 from huggingface_hub import hf_hub_download
 
-# base_model_path = 'SG161222/RealVisXL_V3.0'
-base_model_path = "E:\\github\\stable-diffusion-webui\\models\\Stable-diffusion\\sdxl\\RealVisXL_V3.0.safetensors"
+import modules.default_pipeline as pipeline
+
+base_model_path = pipeline.model_base.filename
 photomaker_ckpt = hf_hub_download(repo_id="TencentARC/PhotoMaker", filename="photomaker-v1.bin", repo_type="model")
 
 def generate_photomaker(prompt, input_id_images, negative_prompt, steps, seed, width, height):
+    print(f"Using base model: {base_model_path} for PhotoMaker")
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     pipe = PhotoMakerStableDiffusionXLPipeline.from_single_file(
         base_model_path,
         torch_dtype=torch.bfloat16,
-        use_safetensors=True,        
-         variant="fp16"
-        # variant=variant
+        use_safetensors=True,                
+        variant="fp16"
     ).to(device)
 
     pipe.load_photomaker_adapter(
@@ -30,10 +31,8 @@ def generate_photomaker(prompt, input_id_images, negative_prompt, steps, seed, w
     )
 
     pipe.id_encoder.to(device)
-
-    # pipe.scheduler = EulerDiscreteScheduler.from_config(pipe.scheduler.config)
+    
     pipe.scheduler =  DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
-    # pipe.scheduler.algorithm_type = 'sde-dpmsolver++'
 
     pipe.fuse_lora()
 
@@ -41,7 +40,6 @@ def generate_photomaker(prompt, input_id_images, negative_prompt, steps, seed, w
     
     bytes_images = []
     for img in input_id_images:
-        print(f"Image type: {type(img)}")
         bytes_images.append(img.tobytes)
 
     images = pipe(
@@ -54,7 +52,7 @@ def generate_photomaker(prompt, input_id_images, negative_prompt, steps, seed, w
         height=height,
         start_merge_step=0,
         generator=generator,
-        guidance_scale=4
+        guidance_scale=4,
     ).images
 
     return images
